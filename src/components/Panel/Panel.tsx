e4c96562434195d3f6bbc05f3b988a34ac16cd35
krystiano13 <crystianotv@gmail.com>
2023-02-26 17:27:25 +0100

    started implementing timer feature

--- src/components/Panel/Panel.tsx
+++ src/components/Panel/Panel.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Panel.css";
 import { Timer } from "./PanelElements/Timer/Timer";
 import { PanelButtons } from "./PanelElements/PanelButtons/PanelButtons";
@@ -9,10 +9,29 @@ interface PanelInterface {
 }
 
 const Panel: React.FC<PanelInterface> = ({ minutes }) => {
+
+  const [seed,setSeed] = useState<number>(0);
+  const [min,setMin] = useState<string>('0');
+  const [sec, setSec] = useState<string>("0");
+
+  let time = 0;
+
+  const handleStart = () => {
+    time = minutes * 60;
+  }
+
+  useEffect(() => {
+    setInterval(() => {
+      if(time > 0) time--;
+      setSec(Math.floor(time % 60).toString());
+      setMin(Math.floor(time / 60).toString())
+    },1000)
+  },[]);
+
   return (
-    <main className="Panel">
-      <Timer minutes={minutes.toString()} seconds="00" />
-      <PanelButtons />
+    <main id={seed.toString()} className="Panel">
+      <Timer minutes={min} seconds={sec}/>
+      <PanelButtons start={handleStart} />
       <ProgressBar />
     </main>
   );
--- src/components/Panel/PanelElements/PanelButtons/PanelButtons.tsx
+++ src/components/Panel/PanelElements/PanelButtons/PanelButtons.tsx
@@ -2,10 +2,14 @@ import React from "react";
 import './PanelButtons.css';
 import skip2 from '../../../../assets/images/skip2.png';
 
-const PanelButtons = () => {
+interface ButtonsInterface {
+  start : () => void
+}
+
+const PanelButtons:React.FC<ButtonsInterface> = ({ start }) => {
     return (
       <section className="PanelButtons">
-        <button className="PanelButtons__start">Start</button>
+        <button onClick={start} className="PanelButtons__start">Start</button>
         <button className="PanelButtons__skip">
           <img className="PanelButtons__skip__img" src={skip2} alt="skip" />
         </button>
